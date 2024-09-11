d2191a70b9044e0ddcba2341a1378c4d32112bc5
Ludmila <krivonosovaludmila088@gmail.com>
2024-09-11 11:32:32 +0300

    Все ошибки устранены.

--- src/processing.py
+++ src/processing.py
@@ -1,34 +1,30 @@
 from typing import Any
+
 my_dict = [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    ]
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
 
 
 def filter_by_state(dictionary_inform: list[dict[str, object]], state_id: str = "EXECUTED") -> list[dict[str, object]]:
     """Функция фильтрующая данные по указаному параметру"""
     list_stat = []
     for i in dictionary_inform:
-        if i ['state'] == state_id:
-         list_stat.append(i)
+        if i["state"] == state_id:
+            list_stat.append(i)
     return list_stat
 
 
-
-def sort_by_date(dictionary_inform:list[dict[str, Any]],revers:bool = True) ->list[dict[str, Any]]:
+def sort_by_date(dictionary_inform: list[dict[str, Any]], revers: bool = True) -> list[dict[str, Any]]:
     """Функция сортирующая словарь по дате в порядке убывания"""
-
 
     sorted_list = sorted(dictionary_inform, key=lambda x: x["date"], reverse=revers)
     return sorted_list
+
 
 if __name__ == "__main__":
     print(filter_by_state(my_dict))
 
     print(sort_by_date(my_dict))
-
-
-
-
